cc2675c353aad985886d4df58b5f40506550136a
Darya K <darya@cs.cmu.edu>
2014-08-26 07:21:51 -0400

    Playing around.
    Reverted the signs in the calculation.

--- eight-queen.py
+++ eight-queen.py
@@ -6,7 +6,7 @@ class BailOut(Exception):
 def validate(queens):
     left = right = col = queens[-1]
     for r in reversed(queens[:-1]):
-        left, right = left - 1, right + 1
+        left, right = left + 1, right - 1
         if r in (left, col, right):
             raise BailOut
 
